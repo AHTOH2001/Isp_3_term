6331ff016a637fb5fc37160a088f690670fe6ca2
AHTOH2001 <antonlapytko@gmail.com>
2020-10-28 13:08:54 +0300

    Added encryptor

--- lab2/lab2/Logger.cs
+++ lab2/lab2/Logger.cs
@@ -6,15 +6,11 @@ using System.Threading;
 
 namespace lab2
 {
-    class Logger
+    static class Logger
     {
 
         static object obj = new object();
         public static string LogFilePath;
-        public Logger(string logFilePath)
-        {
-            LogFilePath = logFilePath;
-        }
 
         static public void RecordEntry(string fileEvent, string filePath)
         {
--- lab2/lab2/Service1.cs
+++ lab2/lab2/Service1.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Security.Cryptography;
+using System.Linq;
+using System.IO;
 
 namespace lab2
 {
     public partial class Service1 //: ServiceBase
     {
-        Logger logger;
         Watcher watcher;
+        //Begin_CONFIG
+        static byte[] aesKey;
+        static byte[] aesIV;
+        //Finish_CONFIG
+        public static void Encrypt(string filePath)
+        {
+            StreamReader streamReader = new StreamReader(filePath);
+            byte[] encrypted = Encryptor.EncryptStringToBytes_Aes(streamReader.ReadToEnd(), aesKey, aesIV);
+            streamReader.Close();
+            StreamWriter streamWriter = new StreamWriter(filePath, false);
+            streamWriter.Write(string.Concat(encrypted.Select(x=>(char)x).ToArray()));            
+            streamWriter.Close();
+            Logger.RecordEntry("зашифрован", filePath);
+        }
+
         //public Service1()
         //{
         //    InitializeComponent();
@@ -19,10 +36,15 @@ namespace lab2
 
         public void OnStart()
         {
-            logger = new Logger(@"D:\Ucheba\Labs\3 sem\Isp\lab2\TargetDirectory\log.txt");
+            var tempAes = Aes.Create();
+            aesKey = tempAes.Key;
+            aesIV = tempAes.IV;
+            Logger.LogFilePath = @"D:\Ucheba\Labs\3 sem\Isp\lab2\TargetDirectory\log.txt";
             watcher = new Watcher(@"D:\Ucheba\Labs\3 sem\Isp\lab2\SourceDirectory");
-            Thread loggerThread = new Thread(new ThreadStart(watcher.Start));
-            loggerThread.Start();
+            Thread watcherThread = new Thread(new ThreadStart(watcher.Start));
+            watcherThread.Start();
+
+
         }
 
         //protected override void OnStop()
